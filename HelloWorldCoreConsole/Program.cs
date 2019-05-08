56be368c6b53eca072942a8caf7039b5d8d554d9
Carlos Guaneme <carlosguaneme@eiu.com>
2019-05-08 16:18:37 -0400

    New message

--- HelloWorldCoreConsole/Program.cs
+++ HelloWorldCoreConsole/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldCoreConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Core World!");
         }
     }
 }
